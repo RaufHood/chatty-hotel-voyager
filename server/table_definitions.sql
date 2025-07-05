aebf9ffdce0c683cc8e930108c5bc4fe0c392983
Aleksei Gridnev <alexeygridnev93@gmail.com>
2025-07-05 23:24:28 +0200

    Create table_definitions.sql

---
+++ server/table_definitions.sql
@@ -0,0 +1,50 @@
+--table definitions in Snowflake:
+
+create or replace TABLE CHATTY_HOTEL_VOYAGER.DBO.HOTELS (
+    HOTEL_ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
+    HOTEL_NAME VARCHAR(255),
+    STARS NUMBER(38,0),
+    RATING FLOAT,
+    LATITUDE FLOAT,
+    LONGITUDE FLOAT,
+    COUNTRY_CODE VARCHAR(5),
+    CITY_CODE VARCHAR(5),
+    HOTEL_TYPE VARCHAR(50),
+    primary key (HOTEL_ID)
+);
+
+create or replace TABLE CHATTY_HOTEL_VOYAGER.DBO.TRIPS (
+    TRIP_ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
+    USER_ID NUMBER(38,0),
+    DATE_START TIMESTAMP_NTZ(9),
+    DATE_END TIMESTAMP_NTZ(9),
+    primary key (TRIP_ID),
+    foreign key (USER_ID) references CHATTY_HOTEL_VOYAGER.DBO.USERS(USER_ID)
+);
+
+create or replace TABLE CHATTY_HOTEL_VOYAGER.DBO.TRIP_LEGS (
+    TRIP_LEG_ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
+    TRIP_ID NUMBER(38,0),
+    USER_ID NUMBER(38,0),
+    HOTEL_ID NUMBER(38,0),
+    ARRIVAL_DATE TIMESTAMP_NTZ(9),
+    DEPARTURE_DATE TIMESTAMP_NTZ(9),
+    STAY_PRICE FLOAT,
+    primary key (TRIP_LEG_ID),
+    foreign key (TRIP_ID) references CHATTY_HOTEL_VOYAGER.DBO.TRIPS(TRIP_ID),
+    foreign key (USER_ID) references CHATTY_HOTEL_VOYAGER.DBO.USERS(USER_ID),
+    foreign key (HOTEL_ID) references CHATTY_HOTEL_VOYAGER.DBO.HOTELS(HOTEL_ID)
+);
+
+create or replace TABLE CHATTY_HOTEL_VOYAGER.DBO.USERS (
+    USER_ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
+    EMAIL VARCHAR(30),
+    NAME VARCHAR(30),
+    SURNAME VARCHAR(30),
+    AUTO_TOKEN VARCHAR(255),
+    primary key (USER_ID)
+);
+
+
+
+
